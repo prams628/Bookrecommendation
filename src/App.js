cc395e034c4a3999d0c884a0eae2bfcd75b706b4
saiprashanth1776 <39609926+saiprashanth1776@users.noreply.github.com>
2020-04-06 10:32:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,52 @@ import logo from './logo.svg'
 import './App.css'
 import Header from "./components/Header"
 import Books from "./components/Books"
+import fire from "./Fire"
+import Login from "./components/Login"
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Books />
-    </div>
-  )
+class App extends React.Component {
+
+	constructor() {
+	    super();
+	    this.state = ({
+	      user: null,
+	    });
+	    this.authListener = this.authListener.bind(this);
+	}
+
+	componentDidMount() {
+    this.authListener();
+  	}
+
+  	authListener() {
+    	fire.auth().onAuthStateChanged((user) => {
+      		console.log(user);
+      		if (user) {
+        		this.setState({ user });
+        		localStorage.setItem('user', user.uid);
+      		} 
+      		else {
+        		this.setState({ user: null });
+        		localStorage.removeItem('user');
+      		}
+    	});
+  	}	
+
+  	render(){
+	  	return (
+	    	<div className="App">
+	    		{this.state.user ? (
+	    			<div>
+	    				<Header />
+	    				<Books />
+	    			</div>
+	    		) :
+	    		(
+	    			<Login />
+	    		)}
+	    	</div>
+	  	)
+	}
 }
 
 export default App;
