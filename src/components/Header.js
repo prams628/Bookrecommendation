bf3e38b3e6048e6a1c377096c3764ed39527ad1b
Prashanth <prashanth.sai99@gmail.com>
2020-04-06 10:35:42 +0530

    login with firebase

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,26 @@
 import React from "react"
+import fire from "../Fire"
 
-function Header(){
-	return(
-		<header>
-			<i className="fa fa-book fa-2x" aria-hidden="true"></i>
-			<h1>Boogle</h1>
-		</header>
-	)
+class Header extends React.Component{
+
+	constructor(props) {
+        super(props);
+        this.logout = this.logout.bind(this);
+    }
+
+    logout() {
+        fire.auth().signOut();
+    }
+
+    render(){
+		return(
+			<header>
+				<i className="fa fa-book fa-2x" aria-hidden="true"></i>
+				<h1>Boogle</h1>
+				<button style={{float: 'right'}} onClick={this.logout}>Logout</button>
+			</header>
+		)
+	}
 }
 
 export default Header
